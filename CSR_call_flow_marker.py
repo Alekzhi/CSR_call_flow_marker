2d0d51a0848878da6dfec0a1ab95e630b7193f60
Alekzhi <141607335+Alekzhi@users.noreply.github.com>
2023-09-07 21:56:12 +0800

    Update CSR_call_flow_marker.py
    Added a global font and default call flow list of sentences.

--- CSR_call_flow_marker.py
+++ CSR_call_flow_marker.py
@@ -3,7 +3,10 @@
 # TODO: Add a file menu to add/edit more call flows
 
 import tkinter as tk
+from tkinter import filedialog
 
+FONT = ("Consolas", 13)
+        
 # Create a function to toggle label state
 def toggle_label_state(label):
     if label["relief"] == "raised":
@@ -11,11 +14,33 @@ def toggle_label_state(label):
     else:
         label.config(relief="raised", fg="black")
 
+
+# Create a function to enable editing
+text_fields = []
+def enable_editing():
+    if not_editing:  # TODO fix enable_editing so that it is active only once
+        not_editing = 0
+        for i, label in enumerate(labels):
+            label.pack_forget()
+            text = tk.Text(root, height=4, width=100, wrap="word", font=FONT)
+            text.insert("1.0", sentences[i])
+            text.pack(fill=tk.BOTH, padx=5, pady=5)
+            text_fields.append(text)
+
+# Create a function to save changes
+def save_changes():
+    for i, text in enumerate(text_fields):
+        sentences[i] = text.get("1.0", "end") # end-1c if excluding last byte (eg: \n)
+        labels[i].config(text=sentences[i])
+        text.pack_forget()
+        labels[i].pack(fill=tk.BOTH)
+    not_editing = 1
+
 # Create the main window
 root = tk.Tk()
 root.title("Customer Support - Call-Flow Marker")
 
-# List of sentences
+# Default list of sentences / Call Flow
 sentences = [
     "[Opening the Call]\n" \
     "Hello and thank you for calling [Company Name] Customer Support.\n" \
@@ -53,10 +78,21 @@ sentences = [
 # Create raised labels for each sentence
 labels = []
 for sentence in sentences:
-    label = tk.Label(root, text=sentence, font=("Consolas", 13),
+    label = tk.Label(root, text=sentence, font=FONT,
                      relief="raised", padx=10, pady=10)
     label.pack(fill=tk.BOTH, side="top", padx=5, pady=5)
     label.bind("<Button-1>", lambda event, label=label: toggle_label_state(label))
     labels.append(label)
+
+# Create a menu
+menu = tk.Menu(root)
+menu.add_command(label="Edit Contents", command=enable_editing)
+root.config(menu=menu)
+
+# Create a File menu
+# file_menu = tk.Menu(menu, tearoff=0)
+# menu.add_cascade(label="File", menu=file_menu)
+# file_menu.add_command(label="Edit Contents", command=enable_editing)
+# file_menu.add_command(label="Save", command=save_changes)
 
 root.mainloop()
